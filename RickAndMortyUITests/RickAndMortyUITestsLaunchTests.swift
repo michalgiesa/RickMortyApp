0d683b7b8c5eef3985703502851961bf520b052a
michalgiesa <91019123+michalgiesa@users.noreply.github.com>
2026-09-08 07:04:45 +0200

    Update RickAndMortyUITestsLaunchTests.swift

--- RickAndMortyUITests/RickAndMortyUITestsLaunchTests.swift
+++ RickAndMortyUITests/RickAndMortyUITestsLaunchTests.swift
@@ -1,10 +1,3 @@
-//
-//  RickAndMortyUITestsLaunchTests.swift
-//  RickAndMortyUITests
-//
-//  Created by Michał Giesa on 01/09/2025.
-//
-
 import XCTest
 
 final class RickAndMortyUITestsLaunchTests: XCTestCase {
